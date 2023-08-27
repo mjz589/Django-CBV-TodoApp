bb11b330caf2e5dbcdde35c425f04717f52d54e1
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-08-28 01:06:05 +0330

    progressing 1.001

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from blog.views import *
+from .views import *
 from blog.feeds import LatestEntriesFeed
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 app_name  = 'blog'
 
 urlpatterns = [
     path ('', indexView, name='fbv-index'),
-    path ('about/', TemplateView.as_view(template_name='about.html')),
+    # path ('about/', TemplateView.as_view(template_name='about.html',extra_context={"name": "alias"}),
+    path ('cbv-index/', IndexView.as_view(), name='cbv-index' ),
+    path ('go-to-index/', RedirectView.as_view(pattern_name="blog:cbv-index"), name='redirect-to-index' ),
+    path ('post/', PostList.as_view(), name='post-list' ),
     # path('' , blog_view, name='index'),
     # path('<int:pid>', blog_single_view, name='single'),
     # path('category/<str:cat_name>' , blog_view, name='category'),
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView ,RedirectView
+from django.views.generic import ListView
+from .models import *
 # Create your views here.
 
 
 def indexView(request):
+    """
+    a function based view to show index page
+    """
     return render(request, 'index.html')
+
+class IndexView(TemplateView):
+    """
+    a class based view to show index page
+    """
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "alias"
+        context["posts"] = Post.objects.all()
+        return context
+    
+class PostList(ListView):
+    model = Post
+    
