fcbc6de037da8d6da9b031245df7aa739aba7364
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-08-28 23:32:30 +0330

    advanced pagination in view

--- core/blog/views.py
+++ core/blog/views.py
@@ -32,10 +32,30 @@ class PostList(ListView):
     """
     # if you don't like the name of object_list for your objects:
     context_object_name = 'posts'
-
+    paginate_by = 2
     def get_queryset(self):
         posts = Post.objects.filter(status=True)
         return posts
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        if not context.get('is_paginated', False):
+            return context
+        # Custom Pagination
+        paginator = context.get('paginator')
+        num_pages = paginator.num_pages
+        current_page = context.get('page_obj')
+        page_no = current_page.number
+
+        if num_pages <= 5 or page_no <= 3:  # case 1 and 2
+            pages = [x for x in range(1, min(num_pages + 1, 5))]
+        elif page_no > num_pages - 3:  # case 4
+            pages = [x for x in range(num_pages - 4, num_pages + 1)]
+        else:  # case 3
+            pages = [x for x in range(page_no - 2, page_no + 3)]
+
+        context.update({'pages': pages})
+        return context
     
 
     
