4b76a2b74d1b9c3fd6cec536c859dbe0c2e0b5fb
Mohamad Javad <mjz589.2018@gmail.com>
2023-08-31 22:14:15 +0330

    Update views.py
    """
    # or you can use it like this:
    
    @api_view()
    def taskDetail(request,id):
        try:
            task = Task.objects.get(pk=id)
            serializer = TaskSerializer(task)
            return Response(serializer.data)
        except Post.DoesNotExist:
            return Response({"detail":"page doesn't exit}, status=404)
    """

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -3,14 +3,18 @@ from rest_framework.response import Response
 from .serializers import TaskSerializer
 from ...models import Task 
 # or instead of ...models you can point models.py like this: todo.models
-
+# from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def taskList(request):
-    return Response({"Mohamad Javad": "Mikey"})
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(tasks.data)
 
 @api_view()
 def taskDetail(request,id):
-    task = Task.objects.get(pk=id)
+    task = get_object_or_404(Task, pk=id)
     serializer = TaskSerializer(task)
     return Response(serializer.data)
+
