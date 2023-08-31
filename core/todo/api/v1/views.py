fc3435cd884bf1fbbae3e030f2ea3cf92fd63c43
Mohamad Javad <mjz589.2018@gmail.com>
2023-09-01 00:58:32 +0330

    api-auth and permission_classes added

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -1,12 +1,16 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view ,permission_classes
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from ...models import Task 
 # or instead of ...models you can point models.py like this: todo.models
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+
+# list of tasks and create a new one
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def taskList(request):
     if request.method == 'GET':
         tasks = Task.objects.all()
@@ -17,8 +21,11 @@ def taskList(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    
+
+
+# show a single task and edit or delete it
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def taskDetail(request,id):
     task = get_object_or_404(Task, pk=id)
     if request.method == 'GET':
