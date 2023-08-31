b81bb35ac1f2f14be0f9d7574f7cf742ee426cae
Mohamad Javad <mjz589.2018@gmail.com>
2023-08-31 22:15:52 +0330

    Update views.py
    serializer must be in response not tasks

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import get_object_or_404
 def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
-    return Response(tasks.data)
+    return Response(serializer.data)
 
 @api_view()
 def taskDetail(request,id):
