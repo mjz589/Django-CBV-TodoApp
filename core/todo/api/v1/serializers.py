959215751dd8f509605de62bc5b1a804e26a6c43
Mohamad Javad <mjz589.2018@gmail.com>
2023-09-02 01:50:55 +0330

    relative url and absolute url added in serializer

--- core/todo/api/v1/serializers.py
+++ core/todo/api/v1/serializers.py
@@ -10,6 +10,14 @@ class TaskSerializer(serializers.ModelSerializer):
     # user must automatically be provided and not be written by users
     user = serializers.PrimaryKeyRelatedField(read_only=True)
 
+    relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name="get_abs_url")
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+    
     class Meta:
         model = Task
-        fields = ('id', 'user', 'title', 'complete',)
+        fields = ('id', 'user', 'title', 'complete', 'relative_url', 'absolute_url', 'created_date')
+
+    
--- core/todo/models.py
+++ core/todo/models.py
@@ -26,7 +26,8 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
-    
+    def get_absolute_api_url(self):
+        return reverse('todo:api-v1:task-detail', kwargs={'pk': self.pk})
     class Meta:
         order_with_respect_to = "user"
         
