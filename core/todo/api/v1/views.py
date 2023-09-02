af2b1245358bf23796cfcc1808c7c364eca569f2
Mohamad Javad <mjz589.2018@gmail.com>
2023-09-02 19:14:26 +0330

    trying to solve conflicts

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -14,15 +14,16 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly , IsAuthenticat
 from rest_framework import serializers
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .paginations import DefaultPagination
 
 class TaskModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filter_backends = [DjangoFilterBackend,SearchFilter, OrderingFilter]
     filterset_fields = ['complete',]
     search_fields = ['title',]
+    ordering_fields = ['created_date']
     # pagination
     pagination_class = DefaultPagination
     
