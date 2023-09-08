838012eec1f98385979c6e7493292a3a84593421
Mohamad Javad <mjz589.2018@gmail.com>
2023-09-08 23:17:47 +0330

    TestTodoView added

--- core/todo/tests/test_todo_view.py
+++ core/todo/tests/test_todo_view.py
@@ -1,12 +1,14 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from accounts.models import User, Profile
+from ..models import Task
+
 
 class TestTodoView(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(email='test@test.com', password='test!1234')
+        self.user = User.objects.create_user(email='test1@test.com', password='test/!1234')
         self.profile = Profile.objects.create(
             user = self.user,
             first_name = 'test_first',
@@ -15,13 +17,23 @@ class TestTodoView(TestCase):
         )
         self.task = Task.objects.create(
             user = self.profile,
-            title = 'test',
-            complete = True,
+            title = 'test1',
+            complete = False,
         )
-    def test_todo_index_url_successful_response(self):
+
+    def test_todo_task_list_anonymouse_response(self):
         url = reverse('todo:task_list') 
         response = self.client.get(url)
         # http response 302 for it redirects to login page
-        self.assertEqual(response.status_code, 302) 
+        self.assertEquals(response.status_code, 302) 
         self.assertTrue(str(response.content).find('task_list'))
-        # self.assertTemplateUsed(response, template_name='todo/task_list.html')
+        
+    def test_todo_task_list_logged_in_response(self):
+        self.client.force_login(self.user)
+        url = reverse('todo:task_list')
+        response = self.client.get(url, follow=True)
+        
+        self.assertRedirects(response, '/accounts/login/?next=/')
+        self.assertEqual(response.status_code, 200)
+            
+        # self.assertTemplateUsed(response, template_name = "todo/task_list.html")
