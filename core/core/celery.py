968c0c19255e5b739a39319956366422fcbf1e33
Mohamad Javad <mjz589.2018@gmail.com>
2023-09-15 22:47:56 +0330

    error celery

--- core/core/celery.py
+++ core/core/celery.py
@@ -6,7 +6,7 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("core")
-
+app.conf.timezone = 'Asia/Tehran'
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
@@ -15,3 +15,13 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+from celery.schedules import crontab
+from todo.tasks import delete_completed_tasks
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(
+        crontab(minute='*/10',),
+        delete_completed_tasks.s(), name='delete completed tasks')
---
+++ core/todo/tasks.py
@@ -0,0 +1,6 @@
+from celery import shared_task
+from .models import Task
+
+@shared_task
+def delete_completed_tasks():
+    return Task.objects.filter(complete=True).delete()
