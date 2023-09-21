4d247a2b7455936102a29e4f67d651ba409f3244
Mohamad Javad <mjz589.2018@gmail.com>
2023-09-21 17:01:02 +0330

    initialized caching

--- core/core/settings.py
+++ core/core/settings.py
@@ -267,3 +267,15 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # celery configs
 CELERY_BROKER_URL = "redis://redis:6379/1"
+
+# caching configs
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        # "TIMEOUT": 300, BY DEFAULT IS 5 MINUTES
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
